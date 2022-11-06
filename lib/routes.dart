20cf5aecace22890bd2507f9083245a19dd36f34
Heischichou <jangarot@gmail.com>
2022-11-06 18:45:01 +0800

    Initial commit

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_intro/screens/login_screen.dart';
+import 'package:flutter_intro/screens/signup_screen.dart';
+import 'package:flutter_intro/screens/dashboard_screen.dart';
+import 'package:flutter_intro/screens/settings_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  Login.routeName: (BuildContext context) => const Login(),
+  Signup.routeName: (BuildContext context) => const Signup(),
+  Dashboard.routeName: (BuildContext context) => const Dashboard(),
+  Settings.routeName: (BuildContext context) => const Settings(),
+};
